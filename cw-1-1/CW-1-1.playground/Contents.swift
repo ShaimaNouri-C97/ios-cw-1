28684b324612e5c1ade5c91c4dea45fe54c29726
ShaimaNouri-308 <66574499+ShaimaNouri-308@users.noreply.github.com>
2020-09-09 19:01:43 +0300

    cw 1 done

---
+++ cw-1-1/CW-1-1.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var name = "shaima"
+var age = 16
+var height = 157
+var hobby1 = "kickboxing"
+var hobby2 = "swimming"
+
+print ("My name is \(name)my age is: \(age), my hobbies are \(hobby1) and \(hobby2)")
+
+var friendsname = "omar"
+var friendsage = 24
+var friendsheight = 162
+var friendshobby1 = "singing"
+var friendshobby2 = "coding"
+
+
+
+
+
