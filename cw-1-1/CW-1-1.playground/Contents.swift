b33a54418e5d2cc90467a2ef41b090c992ea42ff
ShaimaNouri-308 <66574499+ShaimaNouri-308@users.noreply.github.com>
2020-09-09 19:57:32 +0300

    cw 1 done

--- cw-1-1/CW-1-1.playground/Contents.swift
+++ cw-1-1/CW-1-1.playground/Contents.swift
@@ -4,17 +4,20 @@ var name = "shaima"
 var age = 16
 var height = 157
 var hobby1 = "kickboxing"
-var hobby2 = "swimming"
+var hobby2 = "coding"
 
-print ("My name is \(name)my age is: \(age), my hobbies are \(hobby1) and \(hobby2)")
+print ("My name is \(name) my age is: \(age), my hobbies are \(hobby1) and \(hobby2)")
 
-var friendsname = "omar"
-var friendsage = 24
-var friendsheight = 162
-var friendshobby1 = "singing"
-var friendshobby2 = "coding"
+var friendsName = "omar"
+var friendsAge = 24
+var friendsHeight = 180
+var friendsHobby1 = "coding"
+var friendsHobby2 = "singing"
 
+print ("My friends name is \(friendsName) his age is: \(friendsAge), his hobbies are \(friendsHobby1) and \(friendsHobby2)")
 
+var ageDifference = 24 - 16
+print ("the age difference between me and him is \(ageDifference)")
 
-
-
+var mutalHobby = (hobby1 == friendsHobby1) || (hobby1 == friendsHobby2) || (hobby2 == friendsHobby1) || (hobby1 == friendsHobby2)
+print (mutalHobby)
